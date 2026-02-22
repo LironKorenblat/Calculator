0e912ae694c253eb557ca60ce856740e4232b84f
liron_korenblat <lironkorenblat14@gmai.com>
2026-02-22 22:40:22 +0200

    second commit

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.calculator;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,19 +13,38 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button mul;
+    Button dec;
+    Button sub;
+    Button div;
+    Button eq;
+    Button ac;
+    TextView txt;
+    EditText etn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
+        txt = findViewById(R.id.txt);
+        mul = findViewById(R.id.mul);
+        dec = findViewById(R.id.dec);
+        sub = findViewById(R.id.sub);
+        div = findViewById(R.id.div);
+        eq = findViewById(R.id.eq);
+        ac = findViewById(R.id.ac);
+        etn = findViewById(R.id.etn);
 
+    }
+    public int start(){
+        String st1 = etn.getText().toString();
+        int num1 = Integer.parseInt(etn.getText().toString());
+        return num1;
+    }
+    public void restart(View view) {
+        ac.setText("AC");
+        txt.setText("start calculating");
+    }
     public void divide(View view) {
     }
 
@@ -36,8 +58,5 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void equal(View view) {
-    }
-
-    public void restart(View view) {
     }
 }
