474326d0151028b0a8baac181ee5956345da9b04
liron_korenblat <lironkorenblat14@gmai.com>
2026-02-23 08:42:15 +0200

    third commit

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -21,12 +21,16 @@ public class MainActivity extends AppCompatActivity {
     Button ac;
     TextView txt;
     EditText etn;
-
+    int cheak = 0;
+    int num =0;
+    int num2 =0;
+    String option= " ";
+    int cheaker = 0;
+    int reaseult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txt = findViewById(R.id.txt);
         mul = findViewById(R.id.mul);
         dec = findViewById(R.id.dec);
         sub = findViewById(R.id.sub);
@@ -38,25 +42,69 @@ public class MainActivity extends AppCompatActivity {
     }
     public int start(){
         String st1 = etn.getText().toString();
-        int num1 = Integer.parseInt(etn.getText().toString());
+        int num1 = Integer.parseInt(st1);
         return num1;
     }
     public void restart(View view) {
         ac.setText("AC");
-        txt.setText("start calculating");
-    }
-    public void divide(View view) {
-    }
-
-    public void multiply(View view) {
+        num = 0;
+        num2 = 0;
+        option = "";
+        cheak = 1;
+        etn.setText("");
+        etn.setHint("0");
     }
 
     public void plus(View view) {
+        if (cheak == 1) {
+            num = start();
+            option = "+";
+            etn.setText("");
+        }
+    }
+
+    public void multiply(View view) {
+        if (cheak == 1) {
+            num = start();
+            option = "*";
+            etn.setText("");
+        }
+    }
+
+    public void divide(View view) {
+        if (cheak == 1) {
+            num = start();
+            option = "/";
+            etn.setText("");
+        }
     }
 
     public void Subtraction(View view) {
+        if (cheak == 1) {
+            num = start();
+            option = "-";
+            etn.setText("");
+        }
     }
 
     public void equal(View view) {
+        num2 = start();
+
+        if (option == "+") {
+            etn.setText("" + (num + num2));
+        }
+        if (option == "-") {
+            etn.setText("" + (num - num2));
+        }
+        if (option == "*") {
+            etn.setText("" + (num * num2));
+        }
+        if (option == "/") {
+            if (num2 != 0) {
+                etn.setText("" + (num / num2));
+            } else {
+                etn.setText("Error");
+            }
+        }
     }
 }
